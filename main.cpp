34fdfc7a9035d828d63d860b70d696c0f7deaabf
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-05 05:37:20 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   cout << " \n Menampilkan bilangan genap yang ada pada angka 1-20 \n" ;
+    int a=2;
+    while(a<=20){
+        cout << a << endl;
+        a+=2;
+    }
+    cout << "Selesai" << endl;
+    return 0;
+}
